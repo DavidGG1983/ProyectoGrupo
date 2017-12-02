a295cf3a9d693197be295f51ccde264663242dd5
DanielAlba96 <danielalba1996@hotmail.es>
2017-12-02 20:39:33 +0100

    Añadido Hilo para los timers

--- app/src/main/java/com/proyectogrupo/Hilo.java
+++ app/src/main/java/com/proyectogrupo/Hilo.java
@@ -19,7 +19,7 @@ public class Hilo extends Thread {
     @Override
     public void run() {
         try {
-            Thread.sleep(5000);
+            Thread.sleep(time);
             super.run();
         } catch (InterruptedException e) {
             e.printStackTrace();
